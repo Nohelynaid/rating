78c4cc2a5fed0af9b6a42cc00d0046a59e9dc0a3
Nohelynaid <101210341+Nohelynaid@users.noreply.github.com>
2023-07-11 15:31:10 -0600

    Update index.js
    Modified details

--- JS/index.js
+++ JS/index.js
@@ -1,49 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-let selectedNumber; // Variable to store the selected number
+  let selectedNumber; // Variable to store the selected number
 
-function numberRated(event) {//this function will target the content stored in the variable
-  selectedNumber = event.target.textContent; 
-}
+  function numberRated(event) {//this function will target the content stored in the variable
+    selectedNumber = event.target.textContent;
+  }
 
-function submitClicked() { //this function will display the content stored in the variable in the element with id--selectedRate
-  selectedRate.innerHTML = selectedNumber;
-}/*selectedRate.innerHTML is used to access the content of an HTML element with the id selectedRate.
-The content of the HTML element is then updated with the value stored in the variable selectedNumber.*/ 
+  function submitClicked() { //this function will display the content stored in the variable in the element with id--selectedRate
+    selectedRate.innerHTML = selectedNumber;
+  }/*selectedRate.innerHTML is used to access the content of an HTML element with the id selectedRate.
+The content of the HTML element is then updated with the value stored in the variable selectedNumber.*/
 
 
-const buttonElements = document.getElementsByClassName('numbers');
-for (let i = 0; i < buttonElements.length; i++) {
+  const buttonElements = document.getElementsByClassName('numbers');
+
+  for (let i = 0; i < buttonElements.length; i++) {
     const button = buttonElements[i];
 
-  button.addEventListener('click', numberRated);
-
-  button.addEventListener('click', function() {
-    if (button.classList.contains('clicked')) {
-      button.classList.remove('clicked');
-    } else {
-      button.classList.add('clicked');
-    }
-  });
-}
+    button.addEventListener('click', numberRated);
 
 
+    button.addEventListener('click', function () {
+      Array.from(buttonElements).forEach(function (btn) {
+        btn.classList.remove('selected');
+      });
+      button.classList.add('selected');
+    });
+  }
 
-submitBtn.addEventListener('click', submitClicked);
+
+  submitBtn.addEventListener('click', submitClicked);
 
 
   // Define a function to handle the form submission
   function handleSubmit(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
 
-    // Hide the first div and show the second div
-    div1.style.display = 'none';
-    div2.style.display = 'block';
+    if (selectedNumber) { // Check if at least one button is selected
+      div1.style.display = 'none';
+      div2.style.display = 'block';
+    } else {
+      alert("Please select at least one rating");
+    }
   }
 
-  // Show div1 and hide div2 on page load
   div1.style.display = 'block';
   div2.style.display = 'none';
+
+  submitBtn.addEventListener('click', handleSubmit);
+
+});
+
+
+
+
+
+
 
   // Add event listener to the submit button
   submitBtn.addEventListener('click', handleSubmit);
